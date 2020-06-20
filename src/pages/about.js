f2d997289e65833206e6b9e42bfa10849c129615
Rumi Humphrey <rumi@neomantra.net>
2020-06-20 10:49:29 -0400

    minor

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,20 +4,20 @@ import { HelmetDatoCms } from 'gatsby-source-datocms'
 import Img from 'gatsby-image'
 import Layout from "../components/layout"
 
-const About = ({ data: { about } }) => (
+const About = ({ data: { about, spn_about } }) => (
   <Layout>
     <article className="sheet">
       <HelmetDatoCms seo={about.seoMetaTags} />
       <div className="sheet__inner">
-        <h1 className="sheet__title">{about.title}</h1>
-        <p className="sheet__lead">{about.subtitle}</p>
+        <h1 className="sheet__title">{spn_about.title}</h1>
+        <p className="sheet__lead">{spn_about.subtitle}</p>
         <div className="sheet__gallery">
           <Img fluid={about.photo.fluid} />
         </div>
         <div
           className="sheet__body"
           dangerouslySetInnerHTML={{
-            __html: about.bioNode.childMarkdownRemark.html,
+            __html: spn_about.bioNode.childMarkdownRemark.html,
           }}
         />
       </div>
@@ -30,6 +30,40 @@ export default About
 export const query = graphql`
   query AboutQuery {
     about: datoCmsAboutPage {
+      seoMetaTags {
+        ...GatsbyDatoCmsSeoMetaTags
+      }
+      title
+      subtitle
+      photo {
+        fluid(maxWidth: 600, imgixParams: { fm: "jpg", auto: "compress" }) {
+          ...GatsbyDatoCmsSizes
+        }
+      }
+      bioNode {
+        childMarkdownRemark {
+          html
+        }
+      }
+    }
+    spn_about: datoCmsAboutPage(locale: {eq: "es"}) {
+      seoMetaTags {
+        ...GatsbyDatoCmsSeoMetaTags
+      }
+      title
+      subtitle
+      photo {
+        fluid(maxWidth: 600, imgixParams: { fm: "jpg", auto: "compress" }) {
+          ...GatsbyDatoCmsSizes
+        }
+      }
+      bioNode {
+        childMarkdownRemark {
+          html
+        }
+      }
+    }
+    eng_about: datoCmsAboutPage(locale: {eq: "en"}) {
       seoMetaTags {
         ...GatsbyDatoCmsSeoMetaTags
       }
