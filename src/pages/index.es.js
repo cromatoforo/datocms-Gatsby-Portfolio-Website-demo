be6640777841276658c3afcc01e41cd3a8b5e507
Rumi Humphrey <rumi@neomantra.net>
2020-08-03 12:11:18 -0400

    error fixes

--- src/pages/index.es.js
+++ src/pages/index.es.js
@@ -8,82 +8,84 @@ import { Box, Flex, Heading, Button, Text, Card } from 'rebass'
 
 const IndexPage = ({ data }) => (
     <Layout>
-        <LocalizedLink style={{ textDecoration: 'none' }} to={`/store`}>
-            <Flex p={2} flexDirection={['column-reverse', 'row']}>
-                <Box
-                    sx={{ borderTopRightRadius: [0, 0], borderBottomRightRadius: [10, 0], borderTopLeftRadius: [0, 10], borderBottomLeftRadius: [10, 10], height: 220 }}
-                    flex={30}
-                    backgroundColor='#06939B'
-                >
-                    <Flex flexDirection='column'>
-                        <Box color='#eee' p={15}>
-                            <Heading
-                                fontSize={[2, 3]}
-                                p={1}
-                                as='h3'
-                                dangerouslySetInnerHTML={{
-                                    __html: data.home.storeTaglineNode.childMarkdownRemark.html,
-                                }}
-                            />
-                            <Button
-                                mx={1}
-                                my={2}
-                                sx={{
-                                    fontSize: 1,
-                                    borderRadius: 15,
-                                    color: '#40436A',
-                                    backgroundColor: '#FCB515',
-                                }}
+        <Box>
+            <LocalizedLink style={{ textDecoration: 'none' }} to={`/store`}>
+                <Flex p={2} flexDirection={['column-reverse', 'row']}>
+                    <Box
+                        sx={{ borderTopRightRadius: [0, 0], borderBottomRightRadius: [10, 0], borderTopLeftRadius: [0, 10], borderBottomLeftRadius: [10, 10], height: 220 }}
+                        flex={30}
+                        backgroundColor='#06939B'
+                    >
+                        <Flex flexDirection='column'>
+                            <Box color='#eee' p={15}>
+                                <Heading
+                                    fontSize={[2, 3]}
+                                    p={1}
+                                    as='h3'
+                                    dangerouslySetInnerHTML={{
+                                        __html: data.home.storeTaglineNode.childMarkdownRemark.html,
+                                    }}
+                                />
+                                <Button
+                                    mx={1}
+                                    my={2}
+                                    sx={{
+                                        fontSize: 1,
+                                        borderRadius: 15,
+                                        color: '#40436A',
+                                        backgroundColor: '#FCB515',
+                                    }}
+                                >
+                                    {data.home.storeButton}
+                                </Button>
+                            </Box>
+                        </Flex>
+                    </Box>
+                    <Box flex={50}>
+                        <Img className='card__image' style={{ height: 220 }} objectFit='contain' alt={data.home.storeButton} fluid={data.home.storeImage.fluid} />
+                    </Box>
+                </Flex>
+            </LocalizedLink>
+            <Box>
+                <Box p={12} color='#40436A'>
+                    <Text
+                        fontSize={3}
+                        dangerouslySetInnerHTML={{
+                            __html: data.home.homeTaglineNode.childMarkdownRemark.html,
+                        }}
+                    />
+                </Box>
+            </Box>
+            <Box>
+                <Box color='#06939B' p={12} py={0}>
+                    <Heading>{data.home.homeProjects}</Heading>
+                </Box>
+            </Box>
+            <Box>
+                {data.allDatoCmsWork.edges.map(({ node: work }) => (
+                    <LocalizedLink key={work.id} style={{ textDecoration: 'none' }} to={`/projects/${work.slug}`}>
+                        <Flex px={2} py={2} flexDirection={['column-reverse', 'row']}>
+                            <Box
+                                sx={{ borderTopRightRadius: [0, 0], borderBottomRightRadius: [10, 0], borderTopLeftRadius: [0, 10], borderBottomLeftRadius: [10, 10], height: 220 }}
+                                flex={30}
+                                backgroundColor='#40436A'
                             >
-                                {data.home.storeButton}
-                            </Button>
-                        </Box>
-                    </Flex>
-                </Box>
-                <Box flex={50}>
-                    <Img className='card__image' style={{ height: 220 }} objectFit='contain' alt={data.home.storeButton} fluid={data.home.storeImage.fluid} />
-                </Box>
-            </Flex>
-        </LocalizedLink>
-        <Box>
-            <Box p={12} color='#40436A'>
-                <Text
-                    fontSize={3}
-                    dangerouslySetInnerHTML={{
-                        __html: data.home.homeTaglineNode.childMarkdownRemark.html,
-                    }}
-                />
+                                <Flex flexDirection='column'>
+                                    <Box color='#eee' p={15}>
+                                        <Heading fontSize={[2, 3]}>{work.title}</Heading>
+                                        <Text mt={[0, 20]} fontSize={[1, 2]}>
+                                            {work.excerpt}
+                                        </Text>
+                                    </Box>
+                                </Flex>
+                            </Box>
+                            <Box flex={50}>
+                                <Img className='card__image' style={{ height: 220 }} objectFit='contain' alt={work.title} fluid={work.coverImage.fluid} />
+                            </Box>
+                        </Flex>
+                    </LocalizedLink>
+                ))}
             </Box>
-        </Box>
-        <Box>
-            <Box color='#06939B' p={12} py={0}>
-                <Heading>{data.home.homeProjects}</Heading>
-            </Box>
-        </Box>
-        <Box>
-            {data.allDatoCmsWork.edges.map(({ node: work }) => (
-                <LocalizedLink style={{ textDecoration: 'none' }} to={`/projects/${work.slug}`}>
-                    <Flex px={2} py={2} flexDirection={['column-reverse', 'row']}>
-                        <Box
-                            sx={{ borderTopRightRadius: [0, 0], borderBottomRightRadius: [10, 0], borderTopLeftRadius: [0, 10], borderBottomLeftRadius: [10, 10], height: 220 }}
-                            flex={30}
-                            backgroundColor='#40436A'
-                        >
-                            <Flex flexDirection='column'>
-                                <Box color='#eee' p={15}>
-                                    <Heading fontSize={[2, 3]}>{work.title}</Heading>
-                                    <Text mt={[0, 20]} fontSize={[1, 2]}>
-                                        {work.excerpt}
-                                    </Text>
-                                </Box>
-                            </Flex>
-                        </Box>
-                        <Box flex={50}>
-                            <Img className='card__image' style={{ height: 220 }} objectFit='contain' alt={work.title} fluid={work.coverImage.fluid} />
-                        </Box>
-                    </Flex>
-                </LocalizedLink>
-            ))}
         </Box>
     </Layout>
 )
